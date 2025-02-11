0a9638611b0500eee945e5c0dff3fe69dd647ef9
Junior Nascimento <juniorjr10@gmail.com>
2025-02-11 15:33:53 -0300

    Botões de ação finalizados sem integração

--- src/components/PdvActions.jsx
+++ src/components/PdvActions.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
+import PrepararButton from './PrepararButton';
+import CancelarButton from './CancelarButton';
+import FinalizarButton from './FinalizarButton';
 
-const PdvActions = ({ onFinalizar, onCancelar, onPreparar }) => {
+const PdvActions = ({ pdv, setPdv, setOrders, setIsModalOpen, setManagerPassword, setErrorMessage }) => {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 pl-40 flex justify-around z-20">
-      <button
-        onClick={onFinalizar}
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Finalizar
-      </button>
-      <button
-        onClick={onCancelar}
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Cancelar
-      </button>
-      <button
-        onClick={onPreparar}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Preparar
-      </button>
+      <FinalizarButton pdv={pdv} setPdv={setPdv} setOrders={setOrders} />
+      <CancelarButton pdv={pdv} setPdv={setPdv} setOrders={setOrders} setIsModalOpen={setIsModalOpen} />
+      <PrepararButton pdv={pdv} setPdv={setPdv} />
     </div>
   );
 };
